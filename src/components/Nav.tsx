b80b1bb73bbec6bd41e6e6e02d411c3dd559318e
Brent Sullivan <brntsllvn@gmail.com>
2023-08-09 06:16:49 +0000

    fix pdf, add links and guidance

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,7 +2,7 @@ function Nav() {
   return (
     <div className="flex justify-center space-x-4 pt-10">
       <a href="/" className="underline">
-        Home
+        Blog
       </a>
       <a href="/calculator-is-my-startup-viable" className="underline">
         Calculator: Is My Startup Viable?
--- src/components/ViableStartupCalculator.tsx
+++ src/components/ViableStartupCalculator.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { jsPDF } from "jspdf";
 import Nav from "./Nav";
 
 interface Option {
@@ -9,6 +10,9 @@ interface Option {
 interface InputProps {
   label: string;
   subscript: string;
+  guidance: string;
+  readMoreURL: string;
+  readMoreLabel: string;
   options: Option[];
   onResultChange: (result: number) => void;
   onOptionChange: (option: string) => void;
@@ -18,6 +22,9 @@ interface InputProps {
 const InputWithDropdown: React.FC<InputProps> = ({
   label,
   subscript,
+  guidance,
+  readMoreURL,
+  readMoreLabel,
   options,
   onResultChange,
   onOptionChange,
@@ -38,7 +45,14 @@ const InputWithDropdown: React.FC<InputProps> = ({
   return (
     <div className="py-2 pb-1">
       <div>{label}</div>
-      <div className="text-[9px]">{subscript}</div>
+      <div className="text-[9px]">{`Estimate: ${subscript}`}</div>
+      <div className="text-[9px] font-bold">{`Jason's guidance: ${guidance}`}</div>
+      <div className="text-[9px]">
+        Read more:{" "}
+        <a className="underline" href={readMoreURL}>
+          {readMoreLabel}
+        </a>
+      </div>
       <div>
         <select
           value={selectedOption || ""}
@@ -49,7 +63,7 @@ const InputWithDropdown: React.FC<InputProps> = ({
             Select an option
           </option>
           {options.map((option) => (
-            <option key={option.value} value={option.label}>
+            <option className="text-sm" key={option.value} value={option.label}>
               {option.label}
             </option>
           ))}
@@ -73,6 +87,74 @@ const ViableStartupCalculator: React.FC = () => {
     "",
   ]);
   const [businessLabels, setBusinessLabels] = useState<string[]>(["", ""]);
+
+  const downloadPDF = () => {
+    const doc = new jsPDF();
+    let yPos = 20;
+    let yGap = 4;
+    let xPos = 10;
+    let titleSize = 10;
+    let textSize = 8;
+
+    doc.setFontSize(12);
+    doc.text("Is My Startup Viable?", xPos, yPos);
+    yPos += yGap;
+
+    doc.setFontSize(textSize - 3);
+    doc.text(
+      "Jason Cohen's 'Excuse me, is there a problem?' (https://longform.asmartbear.com/problem/)",
+      xPos,
+      yPos
+    );
+    yPos += yGap - 1;
+    doc.text("Calculator (www.adthatch.com/is-my-startup-viable)", xPos, yPos);
+    yPos += yGap - 1;
+    doc.text(
+      "Say hello (https://www.linkedin.com/in/brent-sullivan-350230209/)",
+      xPos,
+      yPos
+    );
+    yPos += yGap + 5;
+
+    doc.setFontSize(titleSize);
+    doc.text("Name of Startup", xPos, yPos);
+    yPos += yGap;
+    doc.setFontSize(textSize);
+    doc.text(businessLabels[0], xPos, yPos);
+    yPos += yGap;
+
+    yPos += yGap;
+    doc.setFontSize(titleSize);
+    doc.text("Startup Idea", xPos, yPos);
+    yPos += yGap;
+    doc.setFontSize(textSize);
+    doc.text(businessLabels[1], xPos, yPos);
+    yPos += yGap;
+
+    optionsArray.forEach((option, index) => {
+      yPos += yGap;
+      doc.setFontSize(titleSize);
+      doc.text(option.label, xPos, yPos);
+      yPos += yGap;
+      doc.setFontSize(textSize);
+      doc.text(selectedOptions[index], xPos, yPos);
+      yPos += yGap;
+    });
+
+    yPos += yGap;
+    doc.setFontSize(titleSize);
+    doc.text(`Score: ${totalResult.toFixed(2)}`, xPos, yPos); // rounded to two decimal places
+    yPos += yGap;
+    doc.setFontSize(textSize);
+    doc.text(`Result: ${calculateResult(totalResult)}`, xPos, yPos);
+    doc.setFontSize(textSize - 3);
+    yPos += yGap + 5;
+    const timestamp =
+      new Date().toISOString().replace("T", " ").split(".")[0] + " UTC";
+    doc.text(`Calculated ${timestamp}`, xPos, yPos);
+
+    doc.save("Startup_Details.pdf");
+  };
 
   const handleInputOptionChange = (index: number, option: string) => {
     setSelectedOptions((prevOptions) => {
@@ -125,10 +207,14 @@ const ViableStartupCalculator: React.FC = () => {
       return "bg-green-400";
     } else if (totalResult >= 2) {
       return "bg-green-200";
+    } else if (totalResult >= 1) {
+      return "bg-red-400";
+    } else if (totalResult >= 0.1) {
+      return "bg-red-500";
     } else if (totalResult === 0) {
       return "bg-stone-200";
     } else {
-      return "bg-red-400";
+      return "bg-red-600";
     }
   };
 
@@ -136,8 +222,11 @@ const ViableStartupCalculator: React.FC = () => {
 
   const optionsArray = [
     {
-      label: "Plausible",
+      label: "Audience Size",
       subscript: "Number of potential customers (consumers or businesses)",
+      guidance: "10M+ people or 100k+ orgs have the problem?",
+      readMoreURL: "https://longform.asmartbear.com/roi-rubric/",
+      readMoreLabel: "Fermi Estimation",
       options: [
         { value: 1000000000, label: "1,000,000,000" },
         { value: 100000000, label: "100,000,000" },
@@ -149,8 +238,12 @@ const ViableStartupCalculator: React.FC = () => {
       ],
     },
     {
-      label: "Self-Aware",
-      subscript: "Willing to solve the problem",
+      label: "Self-Aware Market",
+      subscript: "Does the audience know and care they have the problem?",
+      guidance:
+        "Confirm the problem is real, prospects agree and solving it is a priority",
+      readMoreURL: "https://longform.asmartbear.com/customer-development/",
+      readMoreLabel: "Customer Development",
       options: [
         { value: 0.01, label: "0.01: Few agree or care" },
         {
@@ -165,8 +258,13 @@ const ViableStartupCalculator: React.FC = () => {
       ],
     },
     {
-      label: "Lucrative",
+      label: "Lucrative Market",
       subscript: "Annual allocated budget",
+      guidance:
+        "Confirm the audience has enough money available and budgeted to solve the problem",
+      readMoreURL:
+        "https://longform.asmartbear.com/pricing-determines-your-business-model/",
+      readMoreLabel: "Pricing Determines Your Business Model",
       options: [
         { value: 1000000, label: "$1,000,000" },
         { value: 100000, label: "$100,000" },
@@ -178,11 +276,16 @@ const ViableStartupCalculator: React.FC = () => {
       ],
     },
     {
-      label: "Liquid",
-      subscript: "Frequency of purchase decision",
+      label: "Liquid Market",
+      subscript:
+        "How often do your customers make a purchase decsion and how hard is it to switch?",
+      guidance:
+        "Find all the frictions that prevent purchase (e.g. long-term contract, difficult in moving data, cross-system integrations, etc.)",
+      readMoreURL: "TBD",
+      readMoreLabel: "TBD",
       options: [
-        { value: 0.01, label: "0.01: Every few years" },
-        { value: 0.1, label: "0.1: An annual decision" },
+        { value: 0.01, label: "0.01: Every few years, hard to switch" },
+        { value: 0.1, label: "0.1: Once a year, moderate challenge to switch" },
         {
           value: 1.0,
           label: "1.0: Always in the market, easy to switch",
@@ -190,18 +293,26 @@ const ViableStartupCalculator: React.FC = () => {
       ],
     },
     {
-      label: "Eager (identity)",
+      label: "Eager To Buy From You Specifically?",
       subscript: "Attitude towards your company",
+      guidance:
+        "Do prospects trust you, your product, track record, security standards, customer service, ability to scale, etc.",
+      readMoreURL: "TBD",
+      readMoreLabel: "TBD",
       options: [
         { value: 0, label: "0: They cannot buy from you" },
-        { value: 0.1, label: "0.1: Structural/trust challenges" },
-        { value: 0.5, label: "0.5: Indifferent" },
+        { value: 0.1, label: "0.1: Serious trust challenges" },
+        { value: 0.5, label: "0.5: Indifferent or low-trust product" },
         { value: 1.0, label: "1.0: Emotional desire to select you" },
       ],
     },
     {
-      label: "Eager (comparative)",
+      label: "Eager To Buy From You Versus Competition?",
       subscript: "Competitive differentiation",
+      guidance:
+        "Do you have something unique and does a big chunk of the market care about that thing?",
+      readMoreURL: "https://longform.asmartbear.com/willingness-to-pay/",
+      readMoreLabel: "Willingness to Pay",
       options: [
         { value: 0.1, label: "0.1: No material differentiation" },
         { value: 0.5, label: "0.5: Some best-in-class features" },
@@ -210,18 +321,17 @@ const ViableStartupCalculator: React.FC = () => {
     },
     {
       label: "Enduring",
-      subscript: "Will they still be here a year from now?",
+      subscript: "Will they still be a customer a year from now?",
+      guidance:
+        "5%/mo cancellation means only half the customers will still be customers a year from now. One-time revenue businesses still need repeat revenue",
+      readMoreURL:
+        "https://cloud.substack.com/p/my-top-10-mistakes-in-10-years-gainsight#%C2%A7mistake-not-starting-act-ii-fast-enough",
+      readMoreLabel: "Impossible to become a scale-up unicorn with high churn",
       options: [
-        {
-          value: 0.01,
-          label: "0.01: One-off purchase without loyalty",
-        },
-        { value: 0.1, label: "0.1 : One-off purchase with evangelism" },
-        {
-          value: 0.5,
-          label: "0.5 : Recurring-revenue + recurring-problem",
-        },
-        { value: 1.0, label: "1.0 : Strong lock-in" },
+        { value: 0.01, label: "0.01: One-off purchase without loyalty" },
+        { value: 0.1, label: "0.1: One-off purchase with evangelism" },
+        { value: 0.5, label: "0.5: Recurring revenue or problem" },
+        { value: 1.0, label: "1.0: Strong lock-in" },
       ],
     },
   ];
@@ -275,9 +385,32 @@ const ViableStartupCalculator: React.FC = () => {
         <div
           className={`w-full max-w-md mx-auto px-4 py-8 ${bgColorClass} rounded-lg shadow-md`}
         >
-          <h1 className="text-black text-2xl font-bold text-center mb-4">
+          <h1 className="text-black text-2xl font-bold text-center mb-2">
             Is My Startup Viable?
           </h1>
+          <div className="text-[8px] text-center mb-1">
+            Based on Jason Cohen's{" "}
+            <a
+              className="underline"
+              href="https://longform.asmartbear.com/problem/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Excuse me, is there a problem?
+            </a>
+          </div>
+          <div className="text-[8px] text-center mb-4">
+            See{" "}
+            <a
+              className="underline"
+              href="https://longform.asmartbear.com/roi-rubric/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Fermi Estimation
+            </a>{" "}
+            for why this calculator is useful
+          </div>
           <div className="flex justify-between items-center mt-2 px-4">
             <div className="text-black font-semibold text-sm">
               Score: {totalResult}
@@ -285,10 +418,16 @@ const ViableStartupCalculator: React.FC = () => {
             <div className="text-black font-semibold text-sm">
               Result: {calculateResult(totalResult)}
             </div>
+            <button
+              onClick={downloadPDF}
+              className="text-black font-semibold text-sm border border-gray-300 py-1 px-2 rounded-lg"
+            >
+              Save as PDF
+            </button>
           </div>
           <div>
-            <div className="flex items-center mt-2 px-4 text-xs text-black gap-4">
-              <div>Examples:</div>
+            <div className="flex items-center mt-4 px-4 text-[8px] text-black gap-3">
+              <div>Examples</div>
               {exampleArray.map((example) => (
                 <button
                   className="border border-gray-300 py-1 px-2 rounded-lg"
@@ -312,7 +451,7 @@ const ViableStartupCalculator: React.FC = () => {
                 <label className="block">
                   <span className="text-black">Name of startup</span>
                   <div className="text-[9px] pb-1">
-                    Helpful for capturing the business in one word
+                    Helpful for capturing the business in one(ish) word
                   </div>
                   <input
                     className="form-input block w-full rounded-md bg-white-200 text-black text-sm p-2 h-5"
@@ -342,6 +481,9 @@ const ViableStartupCalculator: React.FC = () => {
                 key={index}
                 label={optionData.label}
                 subscript={optionData.subscript}
+                guidance={optionData.guidance}
+                readMoreURL={optionData.readMoreURL}
+                readMoreLabel={optionData.readMoreLabel}
                 options={optionData.options}
                 onResultChange={(result) =>
                   handleInputResultChange(index, result)
